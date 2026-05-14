7f58a01767d4b18f80042dd29b0b98c96e2a5ef1
mdmuche <abulamartins@gmail.com>
2026-05-14 15:39:04 +0100

    fix: remove dotenv

--- src/middlewares/rateLimit.js
+++ src/middlewares/rateLimit.js
@@ -1,7 +1,4 @@
 import rateLimit from "express-rate-limit";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 export const authLimiter = rateLimit({
   // radix is 10 to ensure the environment variables are parsed as base-10 integers
