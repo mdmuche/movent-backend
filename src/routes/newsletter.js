7a97b955ac5385104c2497edd511ab877d316650
mdmuche <abulamartins@gmail.com>
2026-05-23 16:16:35 +0100

    temp rename for case fix

--- src/routes/newsletter.js
+++ src/routes/newsletter.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-// import { subscribeNewsletter } from "../controllers/newsletter/subscribeNewsletter.js";
+import { subscribeNewsletter } from "../controllers/newsletter/subscribeNewsletter.js";
 import { unsubscribeNewsletter } from "../controllers/newsletter/unsubscribeNewsletter.js";
 import { getSubscribers } from "../controllers/newsletter/getSubscribers.js";
 
@@ -9,18 +9,18 @@ import { verifyToken } from "../middlewares/verifyAuth.js";
 import { rolesAllowed } from "../middlewares/roleBased.js";
 import { checkAccountStatus } from "../middlewares/checkAccountStatus.js";
 
-// import { subscribeNewsletterSchema } from "../validators/newsletter/subscribeNewsletter.js";
+import { subscribeNewsletterSchema } from "../validators/newsletter/subscribeNewsletter.js";
 import { unsubscribeNewsletterSchema } from "../validators/newsletter/unsubscribeNewsletter.js";
 import { getSubscribersSchema } from "../validators/newsletter/getSubscribers.js";
 
 const router = express.Router();
 
 // subscribe user to newsletter
-// router.post(
-//   "/subscribe",
-//   validateRequest(subscribeNewsletterSchema),
-//   subscribeNewsletter,
-// );
+router.post(
+  "/subscribe",
+  validateRequest(subscribeNewsletterSchema),
+  subscribeNewsletter,
+);
 
 // unsubscribe user from newsletter
 router.post(
