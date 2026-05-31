3a76fb4607507e22b017365905c1b5f23b1a60ed
mdmuche <abulamartins@gmail.com>
2026-05-31 16:55:39 +0100

    fix(server): enforce IPv4 DNS resolution to prevent SMTP connection issues

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+import dns from "dns";
+dns.setDefaultResultOrder("ipv4first");
+
 const { connectDB } = await import("./config/connection.js");
 const { default: app } = await import("./app.js");
 
