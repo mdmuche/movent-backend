1a061955716b63ace2c6543ed504eeaa32975b12
mdmuche <abulamartins@gmail.com>
2026-06-01 10:50:51 +0100

    fix: missing uploads

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,11 +1,19 @@
 import multer from "multer";
+import fs from "fs";
 import path from "path";
 import { v4 as uuidv4 } from "uuid";
+
+const uploadDir = path.resolve(process.cwd(), "uploads");
 
 // local storage config
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/");
+    try {
+      fs.mkdirSync(uploadDir, { recursive: true });
+      cb(null, uploadDir);
+    } catch (error) {
+      cb(error);
+    }
   },
 
   filename: function (req, file, cb) {
