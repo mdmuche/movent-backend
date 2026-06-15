07f9c7a7c81f98184b10889d6401096b5d95523c
mdmuche <abulamartins@gmail.com>
2026-06-15 22:49:00 +0100

    allow partial updates for notification preferences

--- src/validators/user/notificationPreference.js
+++ src/validators/user/notificationPreference.js
@@ -1,18 +1,15 @@
 import Joi from "joi";
 
 export const updateNotificationPreferencesSchema = Joi.object({
-  eventUpdates: Joi.boolean().required().messages({
+  eventUpdates: Joi.boolean().messages({
     "boolean.base": "eventUpdates must be a boolean value",
-    "any.required": "eventUpdates is required",
   }),
 
-  promotionalOffers: Joi.boolean().required().messages({
+  promotionalOffers: Joi.boolean().messages({
     "boolean.base": "promotionalOffers must be a boolean value",
-    "any.required": "promotionalOffers is required",
   }),
 
-  securityAlerts: Joi.boolean().required().messages({
+  securityAlerts: Joi.boolean().messages({
     "boolean.base": "securityAlerts must be a boolean value",
-    "any.required": "securityAlerts is required",
   }),
 });
